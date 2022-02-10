ebb2b0174a14d5d9b2f4c10be9e7f8da9a190feb
EShults2124 <shults.eric01@gmail.com>
2022-02-10 11:09:10 -0800

    Test and Makefile

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,10 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -8,7 +14,24 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void inBetween() {
+    public void inBetween() throws IOException {
+
+        String[] test1 = { "hi.com", "hi.com" };
+        String[] args = { "InBetween.md" };
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+
+        boolean error = false;
+
+        for (int i = 0; i < test1.length; i++) {
+            if (test1[i] != links.get(i)) {
+                error = true;
+                break;
+            }
+        }
+
+        assertFalse(error);
 
     }
 
